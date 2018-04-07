e1dea4235e94daff9f0f5812a52833cd2c5ea992
hafiz.zhang <hafiz.zhang@thebeastshop.com>
2018-04-07 21:21:04 +0800

    change xml file

--- src/main/java/com/hafiz/Runner.java
+++ src/main/java/com/hafiz/Runner.java
@@ -1,11 +1,17 @@
 package com.hafiz;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Desc:主类
- * Created by hafiz.zhang on 2017/3/12.
+ * Created by hafiz.zhang on 2018/4/07.
  */
 public class Runner {
     public static void main(String[] args) {
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        System.out.println("context:" + context.getClass());
         System.out.println("The Main Class Is Running....");
     }
 }
